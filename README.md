affee50e0f187b4601a7c53196ce9cdba13627f6
Harry Ho <thienhungho@gmail.com>
2018-11-24 15:54:47 +0700

    Origin Master

---
+++ README.md
@@ -0,0 +1,27 @@
+Yii2 Active Query
+====================
+Active Queryfor Yii2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist thienhungho/yii2-active-query "*"
+```
+
+or add
+
+```
+"thienhungho/yii2-active-query": "*"
+```
+
+to the require section of your `composer.json` file.
+
+Models
+------------
+
+[ActiveQuery](https://github.com/thienhungho/yii2-active-query/tree/master/src/models/ActiveQuery.php)
---
+++ src/models/ActiveQuery.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace thienhungho\ActiveQuery\models;
+
+use yii\db\ActiveQuery as BaseActiveQuery;
+
+/**
+ * Class ActiveQuery
+ * @package thienhungho\ActiveQuery\models
+ */
+class ActiveQuery extends BaseActiveQuery
+{
+    /**
+     * @param $data_type
+     *
+     * @return $this
+     */
+    public function dataType($data_type)
+    {
+        if ($data_type === DATA_TYPE_ARRAY) {
+            $this->asArray();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $id
+     *
+     * @return $this
+     */
+    public function nextRecord($id)
+    {
+        $this->andWhere(['id', '>', $id]);
+
+        return $this;
+    }
+
+    /**
+     * @param $id
+     *
+     * @return $this
+     */
+    public function previousRecord($id)
+    {
+        $this->andWhere(['id', '<', $id]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function latestRecord()
+    {
+        $this->orderBy(['id' => SORT_DESC]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function oldestRecord()
+    {
+        $this->orderBy(['id' => SORT_ASC]);
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     * @return \thienhungho\PostManagement\modules\PostBase\query\Post[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return \thienhungho\PostManagement\modules\PostBase\query\Post|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
